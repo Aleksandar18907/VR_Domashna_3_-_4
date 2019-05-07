4f5ca05b73ec1dd150c2e16c591e42f1bb4ed0c7
Aleksandar <ace_mnp@yahoo.com>
2019-05-08 01:42:02 +0200

    Scene File

---
+++ basicMovement/Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.name == "Teleport")
+            {
+                transform.position = new Vector3(0, 554, 0);
+            }
+        }
+
+    }
+}
---
+++ basicMovement/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+        StartCoroutine(Example());
+
+    }
+    public GameObject can;
+    public GameObject move;
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(5f);
+        can.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
