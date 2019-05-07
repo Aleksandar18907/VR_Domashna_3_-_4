4625aa0b57d70c6019d3309d56723a291a2a8c19
Aleksandar <ace_mnp@yahoo.com>
2019-05-08 00:49:09 +0200

    VR domashna br. 3 i 4

---
+++ Scripts/ExitMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject can;
+    public GameObject move;
+    void Start()
+    {
+
+        StartCoroutine(Example());
+   }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(5f);
+        can.SetActive(true);
+        move.GetComponent<charControl>().enabled = false;
+        move.GetComponent<charMove>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Scripts/PlaySound.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+        StartCoroutine(Example());
+
+    }
+    public GameObject can;
+    public GameObject move;
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(5f);
+        can.SetActive(true);
+        move.GetComponent<CamMovement>().speed = 0;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
